2d02f2f1cb0fd4eba11fa415e60701421223befe
Francisco Lizcano <flizcanor@icloud.com>
2025-04-06 20:40:52 -0500

    se carga podcast

--- src/views/curso/Tema2.vue
+++ src/views/curso/Tema2.vue
@@ -46,7 +46,7 @@
             p(data-aos="fade-left").mb-4.text-white Para estudiar la temática, lo invitamos a escuchar el siguiente podcast.
     
             TarjetaAudio.color-acento-botones.bg-color-white.mb-3(
-              texto="Fundamentos de la innovación estratégica"
+              texto="Tipologías textuales"
               tiempo
               :audio="require('../../assets/curso/podcast/podcast1.mp3')"
             )
